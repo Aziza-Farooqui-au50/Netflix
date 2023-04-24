4b8a47ee49e6c99072cfa9ffd4514f2c5e14fd75
Aziza-Farooqui-au50 <azizafarooqui2@gmail.com>
2023-04-24 08:44:15 +0530

    App.js files updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {createBrowserRouter , RouterProvider , Route , createRoutesFromElements} from 'react-router-dom' ;
 import Login from "./pages/Login";
 import MoviePage from "./pages/Movies";
 import Netflix from "./pages/Netflix";
@@ -9,21 +9,27 @@ import TVShows from "./pages/TVShows";
 import Info from "./pages/Info";
 import UserListedMovies from "./pages/UserListedMovies";
 
-export default function App() {
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+    <Route index element={<Signup />} />
+        <Route  path="/login" element={<Login />} />
+        <Route  path="/player" element={<Player />} />
+        <Route  path="/tv" element={<TVShows />} />
+        <Route  path="/movies" element={<MoviePage />} />
+        <Route  path="/info" element={<Info />} />
+        <Route  path="/new" element={<Player />} />
+        <Route  path="/mylist" element={<UserListedMovies />} />
+        <Route  path="/"  element={<Netflix />} />
+      </Route>
+  )
+)
+
+
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/player" element={<Player />} />
-        <Route exact path="/tv" element={<TVShows />} />
-        <Route exact path="/movies" element={<MoviePage />} />
-        <Route exact path="/info" element={<Info />} />
-        <Route exact path="/new" element={<Player />} />
-        <Route exact path="/mylist" element={<UserListedMovies />} />
-        <Route exact path="/"  element={<Netflix />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
+export default App;
