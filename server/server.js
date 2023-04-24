69c5a6901c966a1fa057f61d743fc70530a685ce
Aziza-Farooqui-au50 <azizafarooqui2@gmail.com>
2023-04-24 09:56:35 +0530

    added

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const db = require('./db.js');
-const path = require("path")
+// const path = require("path")
 const app = express();
 
 require('dotenv').config();
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 //static
-app.use(express.static(path.join(__dirname, "./client/build")))
+// app.use(express.static(path.join(__dirname, "./client/build")))
 
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, "./client/build/index.html"))
-})
+// app.get('*', function(req,res){
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"))
+// })
 db.Database();
 app.use("/api/user", userRoutes);
 
